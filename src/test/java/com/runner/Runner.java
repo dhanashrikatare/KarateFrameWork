3b78beffdf69b885056840e743861550235b07a4
dhanashrikatare <rushikeshkatare706@gmail.com>
2026-04-13 14:58:58 +0530

    updates in createFeature file

--- src/test/java/com/runner/Runner.java
+++ src/test/java/com/runner/Runner.java
@@ -4,12 +4,7 @@ package com.runner;
 import com.intuit.karate.junit5.Karate;
 
 public class Runner {
-//	
-//	@Karate.Test
-//    Karate GetAllBooks() {
-//				return Karate.run("classpath:features/GetAllBooks.feature")
-//				.relativeTo(getClass());
-//	}
+
 	
 	@Karate.Test
 	Karate testPostBooks() {
